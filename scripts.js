df108af0020d930d1103d494e70c4cd214fea781
EricR <rasliche@gmail.com>
2016-01-14 19:26:39 -0500

    No idea what I'm doing

--- scripts.js
+++ scripts.js
@@ -1,57 +1,35 @@
-function solve(a, b, op) {
-  switch (op) {
-    case "addition":
-      return a+b;
-      break;
-    case "subtraction":
-      return a - b;
-      break;
-    case "multiplication":
-      return a * b;
-      break;
-    case "division":
-      return a / b;
-      break;
-  }
-};
-
-// get numbers until an operation is pushed
-// (* or /) should solve on the NEXT operator push
-var primaryOps = ["*", "/"];
-// (+ or -) should solve on the next push unless it's (* or /)
-var secondaryOps = ["+", "-"];
-
-var digits = ["1",'2','3','4','5','6','7','8','9','0'];
-
 $('document').ready( function() {
-  var history = [];
-  var ops = [];
+  var equation = [];
   var tempNum = [0];
   var decimalFlag = false; // true if current number has a decimal
+  // var primaryOps = ["*", "/"];
+  // var secondaryOps = ["+", "-"];
+  var digits = ['0','1','2','3','4','5','6','7','8','9'];
 
   $('.btn').on('click', function() {
-    var clicked = $(this).html();
+    var clicked = $(this);
     console.log(clicked.html());
     // Clear everything
-    if (clicked === "AC") {
+    if (clicked.html() === "AC") {
       history = [];
       ops = [];
       tempNum = [0];
       decimalFlag = false;
       console.log(history, ops, tempNum);
+      $('.display').html('0');
     }
 
-    if (clicked === "." && !decimalFlag) {
+    if (clicked.html() === "." && !decimalFlag) {
       tempNum.push(clicked.html());
       decimalFlag = true;
     }
 
-    if (digits.indexOf(clicked) !== -1) {
+    if (digits.indexOf(clicked.html()) === -1) {
       tempNum.push(clicked.html());
     }
 
     // an operator has been pushed but it wasn't equals, and no other operators
-    if (primaryOps.indexOf(clicked) && ops.length === 0) {
+    if (primaryOps.indexOf(clicked.html()) && ops.length === 0) {
       history.push(parseFloat(tempNum.join('')));
       ops.push(clicked);
       tempNum = [];
